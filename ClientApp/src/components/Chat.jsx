32d13f420611683283654abfa42ce75af06fd4ac
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-20 14:20:16 +0200

    Chat component where the messages are
    Chat component takes sendMessage function as a prop from Room Component

--- ClientApp/src/components/Chat.jsx
+++ ClientApp/src/components/Chat.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 
 const Chat = ({ messages, sendMessage }) => {
-    const [message, setMessage] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
   return (
     <div>
-        {messages.map((m, index) => (
-            <div key={index}>
-                <div>
-                    {m.message}
-                </div>
-                <div>{m.user}</div>
-            </div>
-        ))}
-        <form onSubmit={e => {
-            e.preventDefault();
-            sendMessage(message);
-            setMessage("");
-        }}>
-            <input type="text" placeholder="message" onChange={(e) => setMessage(e.target.value)} value={message}/>
-            <button type="submit">Send</button>
-        </form>
+      {messages.map((m, index) => (
+        <div key={index}>
+          <div>{m.message}</div>
+          <div>{m.user}</div>
+        </div>
+      ))}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage(message);
+          setMessage("");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="message"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
