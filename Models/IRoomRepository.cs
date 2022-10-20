179ebb9ddb67c0b04f2a805d7434a5ada10bdc6b
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-20 11:25:41 +0200

    Models repository

---
+++ Models/IRoomRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApiChatApplication.Models
+{
+    public interface IRoomRepository
+    {
+        IEnumerable<Room> GetAll();
+        Room GetRoomById(int id);
+    }
+}
