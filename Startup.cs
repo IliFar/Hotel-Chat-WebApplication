5d107da55585705e718c996a017b52179cd94fbb
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-20 11:31:08 +0200

    add cors policy services

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,10 @@ namespace WebApiChatApplication
             });
             services.AddScoped<IRoomRepository, MockRoomRepository>();
             services.AddSignalR();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
