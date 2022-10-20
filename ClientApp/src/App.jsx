29ffa5b74f57c5d903a67d5a66411c506ac129aa
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-20 12:20:37 +0200

    route the rooms and a specific room

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Room from "./components/Room";
+import Rooms from "./components/Rooms";
+
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Rooms/>} />
+            <Route path="room/:id" element={<Room />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
