7f6c7f1558bf08ccfbaf9c8087d50ea54e00fcd1
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-20 11:35:14 +0200

    Create Chathub with function to join a room
    Room will be a room from rest api

---
+++ Hubs/Chathub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using WebApiChatApplication.Models;
+
+namespace WebApiChatApplication.Hubs
+{
+    public class Chathub : Hub
+    {
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("RecieveMessage", $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
