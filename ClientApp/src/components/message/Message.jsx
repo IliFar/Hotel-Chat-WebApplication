c9dbc9de966cc289665b78b08d08fe6fb4e35c84
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-25 15:08:21 +0200

    Create Message component as child to Chat component

---
+++ ClientApp/src/components/message/Message.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Message.css";
+
+const Message = ({ messages }) => {
+  const messageRef = React.useRef();
+
+  React.useEffect(() => {
+    if (messageRef && messageRef.current) {
+      const { scrollHeight, clientHeight } = messageRef.current;
+      messageRef.current.scrollTo({
+        left: 0,
+        top: scrollHeight - clientHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
+  return (
+    <div className="messages" ref={messageRef}>
+      {messages.map((m, index) => (
+        <div
+          key={index}
+          className="message-container d-flex flex-column align-items-end"
+        >
+          <div className="message bg-primary p-1 text-white rounded ">
+            {m.message}
+          </div>
+          <div className="user">{m.user}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Message;
