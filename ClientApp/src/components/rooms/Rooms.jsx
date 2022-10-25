136cf8c1bd2a1076deb32f728ec17298118a94c0
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-25 15:06:02 +0200

    Bootstrap update

--- ClientApp/src/components/rooms/Rooms.jsx
+++ ClientApp/src/components/rooms/Rooms.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Rooms.css";
 
 const Rooms = () => {
   const [rooms, setRooms] = React.useState([]);
@@ -21,17 +20,19 @@ const Rooms = () => {
   }, []);
 
   return (
-    <div className="rooms-container">
+    <div className="rooms-container row mt-5 gap-3 h-100 justify-content-center">
       {rooms.map((room, index) => (
         <div
           key={index}
-          className="rooms"
+          className="rooms card text-center col-md-4"
           onClick={() => {
             navigate(`room/${room.id}`);
           }}
         >
-          {room.name}
-          <button className="enter-room">Enter Room</button>
+          <div className="card-body row">
+            <h1 className="card-title">{room.name}</h1>
+          </div>
+          <button className="enter-room btn btn-primary mb-3">Enter Room</button>
         </div>
       ))}
     </div>
