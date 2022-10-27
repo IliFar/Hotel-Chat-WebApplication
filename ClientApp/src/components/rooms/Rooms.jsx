1e8845e85de9cdaa873f886d4f43a599ac3b714a
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-27 17:30:20 +0200

    Update Rooms
    Update rooms with conditional rendering to show a room by id and setting the id state to the clicked roomid

--- ClientApp/src/components/rooms/Rooms.jsx
+++ ClientApp/src/components/rooms/Rooms.jsx
@@ -1,41 +1,40 @@
 import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Room from "../room/Room";
+import { AppContext } from "../../../context/Data";
 
 const Rooms = () => {
-  const [rooms, setRooms] = React.useState([]);
-  const navigate = useNavigate();
-
-  const url = "https://localhost:5001/api/rooms";
-
-  const getRooms = async () => {
-    await axios.get(url).then((res) => {
-      const data = res.data;
-      setRooms(data);
-    });
-  };
-
-  React.useEffect(() => {
-    getRooms();
-  }, []);
+  const { rooms, setId, show, setShow } = React.useContext(AppContext);
 
   return (
-    <div className="rooms-container row mt-5 gap-3 h-100 justify-content-center">
-      
-      {rooms.map((room, index) => (
-        <div
-          key={index}
-          className="rooms card text-center w-75 col-md-4"
-          onClick={() => {
-            navigate(`room/${room.id}`);
-          }}
-        >
-          <div className="card-body row">
-            <h1 className="card-title">{room.name}</h1>
-          </div>
-          <button className="enter-room btn btn-primary mb-3">Enter Room</button>
-        </div>
-      ))}
+    <div className="rooms-container container mt-3 h-100 ">
+      <div className="row justify-content-center gap-3">
+        {!show ? (
+          <>
+            <h1 className="text-white text-center">Enter a room</h1>
+            {rooms.map((room, index) => (
+              <div
+                key={index}
+                className="rooms card text-center w-md-75  col-md-4"
+                onClick={() => {
+                  setShow(true);
+                  setId(room.id);
+                }}
+              >
+                <div className="card-body row">
+                  <h1 className="card-title">{room.name}</h1>
+                </div>
+                <button className="enter-room btn btn-primary mb-3">
+                  Enter Room
+                </button>
+              </div>
+            ))}
+          </>
+        ) : (
+          <Room />
+        )}
+      </div>
     </div>
   );
 };
