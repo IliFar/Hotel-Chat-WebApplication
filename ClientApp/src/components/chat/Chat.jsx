02116e3325f2dd33b91292d7e6752a501914b04c
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-28 08:39:28 +0200

    Add mentions to chat input
    added React Mentions components to display the todos for each room when a user types @

--- ClientApp/src/components/chat/Chat.jsx
+++ ClientApp/src/components/chat/Chat.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { AppContext } from "../../../context/Data";
 import Message from "../message/Message";
+import { MentionsInput, Mention } from "react-mentions";
 import "./Chat.css";
 
 const Chat = () => {
   const { message, messages, setMessage, sendMessage, connection, roomById } =
     React.useContext(AppContext);
+
+    const todos = roomById.todo.map((todo, i) => {
+      return { id: i, display: todo.title };
+    });
+
 
   return (
     <div className="chat container d-flex flex-column bg-white gap-3">
@@ -19,14 +25,18 @@ const Chat = () => {
         }}
       >
         <div className="input-group">
-          <input
+          <MentionsInput
             className="message-input form-control "
             type="text"
             placeholder="message"
             onChange={(e) => setMessage(e.target.value)}
             value={message}
-            contentEditable="true"
-          />
+            onKeyDown={(e) => e.target.value}
+            singleLine={true}
+            inputRef={messageRef}
+          >
+            <Mention trigger="@" data={todos} markup="@__display__" />
+          </MentionsInput>
           <button
             type="submit"
             className="send-btn btn btn-primary"
