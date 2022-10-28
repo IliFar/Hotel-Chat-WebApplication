f781f6e243c7edd23cfd7c0c01a5cdbc54726ff5
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-28 10:03:37 +0200

    Add Luma hotel logo

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -9,6 +9,14 @@ import Data from "../context/Data";
 const App = () => {
   return (
     <div className="app container">
+      <div className="text-center mt-3 mb-3">
+        <img
+          src="https://thelumahotel.com/wp-content/uploads/2021/12/LUMA-LOGO-WORDING-1.png"
+          alt=""
+          className="img"
+        />
+        <hr className="text-white border-top border-5"/>
+      </div>
       <Data>
         <Rooms />
       </Data>
