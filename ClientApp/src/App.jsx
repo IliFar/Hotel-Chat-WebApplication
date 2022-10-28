d6f9cdbf2c36a8461ccbe2be457f585099879c27
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-10-28 10:05:24 +0200

    Delete unused imports

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Room from "./components/room/Room";
 import Rooms from "./components/rooms/Rooms";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 import Data from "../context/Data";
 
 const App = () => {
--- ClientApp/src/components/rooms/Rooms.jsx
+++ ClientApp/src/components/rooms/Rooms.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import Room from "../room/Room";
 import { AppContext } from "../../../context/Data";
 
